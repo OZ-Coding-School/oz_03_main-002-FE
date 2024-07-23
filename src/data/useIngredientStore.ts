27a186b590fd10447813841a859a83264231133d
Yerim1234 <kongsun0805@gmail.com>
2024-07-23 21:57:21 +0900

    Feat: 재료 전역 관리 기능 추가

---
+++ src/data/useIngredientStore.ts
@@ -0,0 +1,22 @@
+import create from 'zustand';
+import { FridgeIngredient } from './ingredientType';
+
+interface IngredientStore {
+  ingredients: FridgeIngredient[];
+  setIngredients: (ingredients: FridgeIngredient[]) => void;
+  updateIngredient: (updatedIngredient: FridgeIngredient) => void;
+}
+
+const useIngredientStore = create<IngredientStore>((set) => ({
+  ingredients: [],
+  setIngredients: (ingredients) => set({ ingredients }),
+  updateIngredient: (updatedIngredient) => set((state) => ({
+    ingredients: state.ingredients.map((ingredient) =>
+      ingredient.fridgeIngreId === updatedIngredient.fridgeIngreId
+        ? updatedIngredient
+        : ingredient
+    ),
+  })),
+}));
+
+export default useIngredientStore;
