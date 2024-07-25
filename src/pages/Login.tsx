f37c6e191d46f9faaf99eaf9b63b8224c5885a27
Skyler85 <worstlie1@gmail.com>
2024-07-25 16:08:18 +0900

    feat: 회원가입 api 로컬 Test - 아이디/이메일로그인 헙체 및 disabled #27

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import SHA256 from 'crypto-js/sha256';
+import { Link } from 'react-router-dom';
 import GoogleIcon from '../assets/google-icon.svg';
 import useAuthStore from '../store/useAuthStore';
-import { Link } from 'react-router-dom';
 
 function Login() {
   const [id, setId] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isEmailLogin, setIsEmailLogin] = useState(false);
+
   const login = useAuthStore((state) => state.login);
+  const emailLogin = useAuthStore((state) => state.emailLogin);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -15,13 +19,13 @@ function Login() {
     // 암호화 적용 - SHA256 단방향 해시 적용
     const hashedPassword = SHA256(password).toString();
     console.log(id, hashedPassword);
+    if (isEmailLogin === true) {
+      alert('기능을 개발중입니다. 아이디로 로그인 해 주세요');
+    }
     login(id, hashedPassword);
   };
 
   const handleGoogleLogin = () => {};
-
-  const handleEmailLogin = () => {};
-
   return (
     <div id="signup-container">
       <div className="flex flex-col justify-center py-3 px-8 mt-10">
@@ -34,24 +38,49 @@ function Login() {
       <div className="py-8 ml-5 mr-5 px-4">
         <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
-            <label
-              htmlFor="username"
-              className="block text-sm mr-10 font-bold text-gray-700"
-            >
-              아이디
-            </label>
-            <div className="mt-1">
-              <input
-                id="user-id"
-                name="user-id"
-                type="text"
-                required
-                placeholder="아이디를 입력해주세요."
-                className="appearance-none block w-full h-12 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-lg"
-                value={id}
-                onChange={(e) => setId(e.target.value)}
-              />
-            </div>
+            {isEmailLogin ? (
+              <>
+                <label
+                  htmlFor="email"
+                  className="block text-sm mr-10 font-bold text-gray-700"
+                >
+                  이메일
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="text"
+                    required
+                    placeholder="이메일을 입력해주세요."
+                    className="appearance-none block w-full h-12 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-lg"
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                  />
+                </div>
+              </>
+            ) : (
+              <>
+                <label
+                  htmlFor="id"
+                  className="block text-sm mr-10 font-bold text-gray-700"
+                >
+                  아이디
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="id"
+                    name="id"
+                    type="text"
+                    required
+                    placeholder="아이디를 입력해주세요."
+                    className="appearance-none block w-full h-12 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-lg"
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                  />
+                </div>
+              </>
+            )}
           </div>
           <div>
             <label
@@ -94,13 +123,24 @@ function Login() {
 
         <div className="mt-6">
           <div className="flex justify-between mx-6 my-8">
-            <button
-              type="button"
-              onClick={handleEmailLogin}
-              className="text-sm font-medium  text-sky-600 hover:text-sky-500 underline"
-            >
-              이메일로 로그인
-            </button>
+            {isEmailLogin ? (
+              <button
+                type="button"
+                onClick={() => setIsEmailLogin(false)}
+                className="text-sm font-medium  text-sky-600 hover:text-sky-500 underline"
+              >
+                아이디로 로그인
+              </button>
+            ) : (
+              <button
+                disabled
+                type="button"
+                onClick={() => setIsEmailLogin(true)}
+                className="text-sm font-medium text-slate-400"
+              >
+                이메일로 로그인
+              </button>
+            )}
             <button
               disabled
               type="button"
@@ -117,7 +157,7 @@ function Login() {
           <div className="mt-12 flex justify-center items-center space-x-2">
             <span className="text-sm text-gray-500">오늘 처음오셨나요?</span>
             <Link
-              to={'/signup'}
+              to="/signup"
               className="text-sm font-medium text-sky-600 hover:text-sky-500 underline"
             >
               회원가입
