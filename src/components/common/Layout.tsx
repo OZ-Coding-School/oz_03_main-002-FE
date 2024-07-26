24565c32770680be2edf6f860a7081f67a6f6e6a
Skyler85 <worstlie1@gmail.com>
2024-07-26 15:50:06 +0900

    Fix: 레이아웃 반응형 불일치 오류 수정 #47

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -7,7 +7,7 @@ function Layout() {
       className=" bg-gray-100 flex flex-col justify-center "
       style={{ height: 'calc(100vh - 76px)' }} // TODO 해당 높이는 추후 App.tsx 파일 내 header코드 제거시에 수정 예정
     >
-      <div className="relative h-dvh sm:h-[800px] md:h-[800px] w-full sm:w-full md:w-[360px] bg-white shadow-2xl sm:rounded-3xl md:rounded-3xl mx-auto min-w-[360px] overflow-hidden">
+      <div className="relative h-dvh sm:h-[800px] md:h-[800px] w-full sm:w-[360px] md:w-[360px] bg-white shadow-2xl sm:rounded-3xl md:rounded-3xl mx-auto min-w-[360px] overflow-hidden">
         <GlobalHeader />
         <Outlet />
       </div>
