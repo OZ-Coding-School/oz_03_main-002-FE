dfe954bd1fb66230d79268b753cbe2f7ea155ef4
Yerim1234 <kongsun0805@gmail.com>
2024-08-01 12:21:44 +0900

    Feat: 구글 로그인 기능 추가

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -70,7 +70,9 @@ const useAuthStore = create<AuthState & AuthActions>()(
         googleLogin: async () => {
           set({ loading: true, error: '' });
           try {
-            const response = await axios.post<AuthResponse>('/oauth/google');
+            const response = await axios.post<AuthResponse>(
+              'https://api.naengttogi.com/api/v1/google/login/',
+            );
             set({
               token: response.data.token,
               loading: false,
