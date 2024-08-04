677e4482e8a43a297a31c972895ac2af1d40cedb
Yerim1234 <kongsun0805@gmail.com>
2024-08-05 01:53:19 +0900

    Chore: 배포 오류 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import useAuthStore from '../store/useAuthStore';
 
 const instance = axios.create({
-  baseURL: 'https://api.naengttogi.com/',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,10 @@
 // declarations.d.ts
 declare module 'crypto-js';
 declare module 'uuid';
+
+declare module 'crypto-js/sha256' {
+  import { Hash } from 'crypto-js';
+
+  const sha256: (message: string | any) => Hash;
+  export = sha256;
+}
